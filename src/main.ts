eefe26bf65a1402517b3ce2cd3a25a6e14031569
vicse <vicseore@gmail.com>
2025-01-30 23:12:57 -0500

    feat: config environment variables

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './common/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(envs.port);
 }
 bootstrap();
